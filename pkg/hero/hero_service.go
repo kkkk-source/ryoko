20f3361246fe33cfb07dca47ede2e75ba5778d62
k <mol.tired@gmail.com>
2021-04-16 01:31:22 -0500

    Add Update and Destroy

--- pkg/hero/hero_service.go
+++ pkg/hero/hero_service.go
@@ -10,11 +10,25 @@ func NewHeroService(repo HeroRepository) *HeroService {
 	}
 }
 
+func (s *HeroService) Save(hero *Hero) (*Hero, error) {
+	heroCreated, err := s.repo.Store(hero)
+	return heroCreated, err
+}
+
 func (s *HeroService) Find(id int) (*Hero, error) {
 	hero, err := s.repo.Find(id)
 	return hero, err
 }
 
 func (s *HeroService) FindAll() ([]*Hero, error) {
-	return s.repo.FindAll()
+	heroes, err := s.repo.FindAll()
+	return heroes, err
+}
+
+func (s *HeroService) Update(hero *Hero) error {
+	return s.repo.Update(hero)
+}
+
+func (s *HeroService) Destroy(id int) error {
+	return s.repo.Destroy(id)
 }
