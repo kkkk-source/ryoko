156e349b87d438b52f62587b06773d00b410eec9
k <mol.tired@gmail.com>
2021-04-16 01:33:45 -0500

    Add PUT, DELETE, and POST methods

--- cmd/main.go
+++ cmd/main.go
@@ -15,7 +15,10 @@ func main() {
 	heroHandler := handler.NewHeroHandler(heroService)
 
 	r := mux.NewRouter()
+	r.HandleFunc("/heroes/{ID:[0-9]+}", heroHandler.DeleteHero).Methods(http.MethodDelete)
 	r.HandleFunc("/heroes/{ID:[0-9]+}", heroHandler.GetHero).Methods(http.MethodGet)
 	r.HandleFunc("/heroes", heroHandler.GetHeroes).Methods(http.MethodGet)
+	r.HandleFunc("/heroes", heroHandler.AddHero).Methods(http.MethodPost)
+	r.HandleFunc("/heroes", heroHandler.UpdateHero).Methods(http.MethodPut)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
